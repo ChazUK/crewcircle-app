ff3237f5fd81c94afc1271a235aeeac5524feb1f
Charlie Francis <git@charliefrancis.dev>
2026-04-22 20:46:26 +0100

    chore: update dependencies

--- .rnstorybook/main.ts
+++ .rnstorybook/main.ts
@@ -3,6 +3,9 @@ import type { StorybookConfig } from "@storybook/react-native";
 const main: StorybookConfig = {
   stories: ["../src/components/**/*.stories.?(ts|tsx|js|jsx)"],
   addons: ["@storybook/addon-ondevice-controls", "@storybook/addon-ondevice-actions"],
+  core: {
+    disableTelemetry: true,
+  },
 };
 
 export default main;
