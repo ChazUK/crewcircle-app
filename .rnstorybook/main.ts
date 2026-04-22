e44e99b8a31b37cf0bdc210412ccaec450134b71
Charlie Francis <git@charliefrancis.dev>
2026-04-22 22:38:00 +0100

    feat: adding native tab navigation

--- .rnstorybook/main.ts
+++ .rnstorybook/main.ts
@@ -3,9 +3,6 @@ import type { StorybookConfig } from "@storybook/react-native";
 const main: StorybookConfig = {
   stories: ["../src/components/**/*.stories.?(ts|tsx|js|jsx)"],
   addons: ["@storybook/addon-ondevice-controls", "@storybook/addon-ondevice-actions"],
-  core: {
-    disableTelemetry: true,
-  },
 };
 
 export default main;
